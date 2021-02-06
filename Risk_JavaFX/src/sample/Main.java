8ac4b5367d008171e3a5631d9719a4df9745bb25
DenzelVvs <denzelvivas2108@gmail.com>
2021-02-06 21:11:11 +0000

    Made .jar file

--- Risk_JavaFX/src/sample/Main.java
+++ Risk_JavaFX/src/sample/Main.java
@@ -1,31 +1,7 @@
 package sample;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
-
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-//        Pane input = FXMLLoader.load(getClass().getResource("input.fxml"));
-//        root.getChildren().add(input);
-        Scene scene = new Scene(root);
-        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-        primaryStage.setTitle("Risk");
-        primaryStage.setScene(scene);
-        primaryStage.setResizable(false);
-        primaryStage.show();
-    }
-
-
+public class Main {
     public static void main(String[] args) {
-
-        launch(args);
+        Risk.main(args);
     }
 }
