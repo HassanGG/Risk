5f041238dd12635ecbd6236c73a0ec84af79463f
HassanGG <hassan.g.ghnewa@gmail.com>
2021-02-06 13:51:48 +0000

    finished input output for getting player1 and player 2 names and assigning countries

--- Risk_JavaFX/src/sample/Controller.java
+++ Risk_JavaFX/src/sample/Controller.java
@@ -1,8 +1,14 @@
 package sample;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -14,8 +20,16 @@ public class Controller implements Initializable {
             eastAustralia, newGuinea, westAustralia, indonesia, venezuela, peru, brazil, argentina,
             congo, northAfrica, southAfrica, egypt, eastAfrica, madagascar;
 
-
     public final int[] continentIDs = Constants.getContinentIds();
+
+    //global variables for io.
+    @FXML private TextArea outputText;
+    @FXML private TextField inputText;
+    private String strInput;
+    private String player1, player2;
+    Allocate allocate;
+    private String listOutput = "";
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -37,5 +51,39 @@ public class Controller implements Initializable {
                 case 5 -> countryButtons[index].setStyle("-fx-background-color: #ffbb00; -fx-text-fill: #000000");  //Africa
             }
         }
+
+        listOutput += "Please enter name for Player 1\n";
+        outputText.setText(listOutput);
+        inputText.setOnAction(e ->{
+            strInput = inputText.getText();
+            parseInput();
+        });
+
     }
+
+    //integer that decides what to do with input (idk how to do this in better way)
+    private int i = 0;
+    private void parseInput() {
+        switch (i) {
+            case 0 -> {
+                //gets player 1 name from input
+                player1 = strInput;
+                listOutput += "Please enter name for Player 2\n";
+                outputText.setText(listOutput);
+                inputText.setText("");
+            }
+            case 1 -> {
+                //assigns countries to each player after getting player 2 name
+                player2 = strInput;
+                listOutput += "player1: " + player1 + "\nplayer2: " + player2 + "\n";
+                inputText.setText("");
+                allocate = new Allocation(player1, player2);
+                allocate.assignCountries();
+                outputText.setText(listOutput);
+
+            }
+        }
+        i++;
+    }
+
 }
